4663a9ec60df95f8499fdcccff2c9bcbc449d5cc
annalatyeva <anysha_s@mail.ru>
2024-08-04 17:41:52 +0500

    add styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
-import {ToDoList} from "./pages/ToDoList";
+import "./assets/scss/normalize.scss";
+import "./assets/scss/style.scss";
+import {ToDoListPage} from "./pages/ToDoListPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ToDoList></ToDoList>
+		<ToDoListPage />
 	</React.StrictMode>
 );
