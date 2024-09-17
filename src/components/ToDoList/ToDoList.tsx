feb57e68884e442aa758e9063b5bd63c95b5efce
annalatyeva <anysha_s@mail.ru>
2024-09-18 00:35:30 +0500

    add toDo arr and components

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,22 +1,38 @@
 import {ToDoListItem} from "./ToDoListItem/ToDoListItem";
 import "./ToDoList.scss";
+import {ToDo} from "../../models/todo-item";
 
-export const ToDoList = () => {
+export const ToDoList = (props: {todos: ToDo[]}) => {
+	const checkedList = () => {
+		return props.todos
+			.filter((item) => !item.isDone)
+			.map((item, idx) => {
+				return (
+					<ToDoListItem
+						toDoItem={item}
+						key={idx}
+					/>
+				);
+			});
+	};
+
+	const unCheckedList = () => {
+		return props.todos
+			.filter((item) => item.isDone)
+			.map((item, idx) => {
+				return (
+					<ToDoListItem
+						toDoItem={item}
+						key={idx}
+					/>
+				);
+			});
+	};
+
 	return (
 		<div className="todo-container">
-			<ul className="todo-list failed">
-				<ToDoListItem />
-			</ul>
-			<ul className="todo-list completed">
-				<ToDoListItem />
-				{/* <li className="todo-list-item__wrapper">
-					<span>Вторая задача</span>
-					<div className="todo-list-item__buttons">
-						<button className="btn-trash"></button>
-						<button className="btn-uncheck"></button>
-					</div>
-				</li> */}
-			</ul>
+			<ul className="todo-list failed">{checkedList()}</ul>
+			<ul className="todo-list completed">{unCheckedList()}</ul>
 		</div>
 	);
 };
--- src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
+++ src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
@@ -1,12 +1,13 @@
 import "./ToDoListItem.scss";
+import {ToDo} from "../../../models/todo-item";
 
-export const ToDoListItem = () => {
+export const ToDoListItem = (props: {toDoItem: ToDo}) => {
 	return (
 		<li className="todo-list-item__wrapper">
-			<span>Первая задача</span>
+			<span>{props.toDoItem.text}</span>
 			<div className="todo-list-item__buttons">
 				<button className="btn-trash"></button>
-				<button className="btn-check"></button>
+				<button className={props.toDoItem.isDone ? "btn-check" : "btn-uncheck"}></button>
 			</div>
 		</li>
 	);
