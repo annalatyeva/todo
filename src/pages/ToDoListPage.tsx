1fa4e1d1a8415a849f70ccee0e236eac34eb77b3
annalatyeva <anysha_s@mail.ru>
2024-09-25 08:58:51 +0500

    logic for add newToDos

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,10 +1,11 @@
+import {useState} from "react";
 import {Form} from "../components/Form/Form";
 import {Header} from "../components/Header/Header";
 import {ToDoList} from "../components/ToDoList/ToDoList";
 import {ToDo} from "../models/todo-item";
 
 export const ToDoListPage = () => {
-	const todos: ToDo[] = [
+	const [todos, setTodos] = useState<ToDo[]>([
 		{
 			id: 0,
 			text: "Первая задача",
@@ -25,12 +26,22 @@ export const ToDoListPage = () => {
 			text: "Четвертая задача",
 			isDone: true,
 		},
-	];
+	]);
+
+	const createNewToDo = (text: string) => {
+		let newToDo: ToDo = {
+			id: todos.length,
+			text: text,
+			isDone: false,
+		};
+
+		setTodos([...todos, newToDo]);
+	};
 
 	return (
 		<>
 			<Header />
-			<Form />
+			<Form createNewToDo={createNewToDo} />
 			<ToDoList todos={todos} />
 		</>
 	);
